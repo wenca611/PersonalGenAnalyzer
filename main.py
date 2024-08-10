e0c4d9ea8955c5ec653008be70ba5f76183777d0
venca611 <wenca611@seznam.cz>
2024-08-10 10:35:07 +0200

    main v1.1

--- main.py
+++ main.py
@@ -1,108 +1,133 @@
 """
 Generátor a analyzátor osobních údajů s detekcí shod a oslavenců
 
-
+https://www.mvcr.cz/clanek/seznam-rodove-neutralnich-jmen.aspx
+https://web.archive.org/web/20160307034828/http://www.mvcr.cz/clanek/cetnost-jmen-a-prijmeni-722752.aspx?q=Y2hudW09MQ%3D%3D
 """
-# import pandas as pd
+import pandas as pd
 # import random
 # from datetime import datetime, timedelta
 # import unidecode
 
+#
+# class DataGenerator:
+#     def __init__(self, first_names_file, last_names_file):
+#         self.first_names = pd.read_excel(first_names_file)['Jména'].tolist()
+#         self.last_names = pd.read_excel(last_names_file)['Příjmení'].tolist()
+#         self.titles = ['Ing.', 'Mgr.', 'PhDr.', 'MUDr.', 'JUDr.', 'RNDr.', 'doc.', 'prof.']
+#
+#     def generate_name(self):
+#         # Logika pro generování jmen s různými variacemi
+#         pass
+#
+#     def generate_date(self):
+#         # Logika pro generování náhodného data
+#         pass
+#
+#     def generate_record(self):
+#         # Logika pro generování celého záznamu
+#         pass
+#
+#     def generate_dataset(self, num_records):
+#         # Generování celého datasetu
+#         pass
+#
+#
+# class DataCleaner:
+#     @staticmethod
+#     def remove_diacritics(text):
+#         return unidecode.unidecode(text)
+#
+#     @staticmethod
+#     def remove_titles(text):
+#         # Logika pro odstranění titulů
+#         pass
+#
+#     @staticmethod
+#     def remove_special_characters(text):
+#         # Logika pro odstranění speciálních znaků
+#         pass
+#
+#     @staticmethod
+#     def clean_record(record):
+#         # Aplikace všech čistících metod na záznam
+#         pass
+#
+#
+# class MatchDetector:
+#     @staticmethod
+#     def detect_full_match(record):
+#         # Logika pro detekci plné shody
+#         pass
+#
+#     @staticmethod
+#     def detect_partial_match(record):
+#         # Logika pro detekci částečné shody
+#         pass
+#
+#
+# class CelebrationFinder:
+#     def __init__(self, name_days_file):
+#         self.name_days = pd.read_excel(name_days_file)
+#
+#     def find_birthdays(self, dataset, start_date):
+#         # Logika pro nalezení narozenin
+#         pass
+#
+#     def find_name_days(self, dataset, start_date):
+#         # Logika pro nalezení svátků
+#         pass
+#
+#
+# class MainProcessor:
+#     def __init__(self, first_names_file, last_names_file, name_days_file):
+#         self.generator = DataGenerator(first_names_file, last_names_file)
+#         self.cleaner = DataCleaner()
+#         self.matcher = MatchDetector()
+#         self.celebration_finder = CelebrationFinder(name_days_file)
+#
+#     def run(self, num_records):
+#         # Hlavní logika pro spuštění celého procesu
+#         raw_data = self.generator.generate_dataset(num_records)
+#         cleaned_data = [self.cleaner.clean_record(record) for record in raw_data]
+#         matched_data = [
+#             {**record,
+#              'full_match': self.matcher.detect_full_match(record),
+#              'partial_match': self.matcher.detect_partial_match(record)}
+#             for record in cleaned_data
+#         ]
+#         birthdays = self.celebration_finder.find_birthdays(matched_data, datetime.now())
+#         name_days = self.celebration_finder.find_name_days(matched_data, datetime.now())
+#
+#         # Uložení výsledků
+#         return matched_data, birthdays, name_days
+#
+#
+# if __name__ == "__main__":
+#     processor = MainProcessor('jmena.xlsx', 'prijmeni.xlsx', 'svatky.xlsx')
+#     results, birthdays, name_days = processor.run(10000)
+#     # Zde můžete přidat kód pro uložení výsledků do souboru nebo jejich další zpracování
 
-class DataGenerator:
-    def __init__(self, first_names_file, last_names_file):
-        self.first_names = pd.read_excel(first_names_file)['Jména'].tolist()
-        self.last_names = pd.read_excel(last_names_file)['Příjmení'].tolist()
-        self.titles = ['Ing.', 'Mgr.', 'PhDr.', 'MUDr.', 'JUDr.', 'RNDr.', 'doc.', 'prof.']
 
-    def generate_name(self):
-        # Logika pro generování jmen s různými variacemi
+class DataProcessor:
+    def __init__(self):
+        self.data = pd.DataFrame()
+
+    def load_first_name(self, *files):
+        all_names = []
+        for file in files:
+            df = pd.read_excel(file)
+            if 'Name' in df.columns:
+                all_names.append(df[['Name']])
+            else:
+                print(f"Warning: 'Name' column not found in {file}.")
+
+        self.data = pd.concat(all_names, ignore_index=True)
+
+
+    def load_last_name(self, file):
         pass
-
-    def generate_date(self):
-        # Logika pro generování náhodného data
-        pass
-
-    def generate_record(self):
-        # Logika pro generování celého záznamu
-        pass
-
-    def generate_dataset(self, num_records):
-        # Generování celého datasetu
-        pass
-
-
-class DataCleaner:
-    @staticmethod
-    def remove_diacritics(text):
-        return unidecode.unidecode(text)
-
-    @staticmethod
-    def remove_titles(text):
-        # Logika pro odstranění titulů
-        pass
-
-    @staticmethod
-    def remove_special_characters(text):
-        # Logika pro odstranění speciálních znaků
-        pass
-
-    @staticmethod
-    def clean_record(record):
-        # Aplikace všech čistících metod na záznam
-        pass
-
-
-class MatchDetector:
-    @staticmethod
-    def detect_full_match(record):
-        # Logika pro detekci plné shody
-        pass
-
-    @staticmethod
-    def detect_partial_match(record):
-        # Logika pro detekci částečné shody
-        pass
-
-
-class CelebrationFinder:
-    def __init__(self, name_days_file):
-        self.name_days = pd.read_excel(name_days_file)
-
-    def find_birthdays(self, dataset, start_date):
-        # Logika pro nalezení narozenin
-        pass
-
-    def find_name_days(self, dataset, start_date):
-        # Logika pro nalezení svátků
-        pass
-
-
-class MainProcessor:
-    def __init__(self, first_names_file, last_names_file, name_days_file):
-        self.generator = DataGenerator(first_names_file, last_names_file)
-        self.cleaner = DataCleaner()
-        self.matcher = MatchDetector()
-        self.celebration_finder = CelebrationFinder(name_days_file)
-
-    def run(self, num_records):
-        # Hlavní logika pro spuštění celého procesu
-        raw_data = self.generator.generate_dataset(num_records)
-        cleaned_data = [self.cleaner.clean_record(record) for record in raw_data]
-        matched_data = [
-            {**record,
-             'full_match': self.matcher.detect_full_match(record),
-             'partial_match': self.matcher.detect_partial_match(record)}
-            for record in cleaned_data
-        ]
-        birthdays = self.celebration_finder.find_birthdays(matched_data, datetime.now())
-        name_days = self.celebration_finder.find_name_days(matched_data, datetime.now())
-
-        # Uložení výsledků
-        return matched_data, birthdays, name_days
-
 
 if __name__ == "__main__":
-    processor = MainProcessor('jmena.xlsx', 'prijmeni.xlsx', 'svatky.xlsx')
-    results, birthdays, name_days = processor.run(10000)
-    # Zde můžete přidat kód pro uložení výsledků do souboru nebo jejich další zpracování
+    processor = DataProcessor()
+    processor.load_first_name("Seznam_muzskych_jmen_-_20240731")
